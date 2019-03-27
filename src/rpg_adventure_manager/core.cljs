0ff6a5df879fd66704ae489487091b2b15093ffa
peterjewicz <peterjewicz@totalwebconnections.com>
2019-03-27 08:14:37 -0500

    loading initial state

--- src/rpg_adventure_manager/core.cljs
+++ src/rpg_adventure_manager/core.cljs
@@ -6,21 +6,21 @@
               [rpg-adventure-manager.newlocation :as newlocation]
               [rpg-adventure-manager.newhook :as newhook]
               [rpg-adventure-manager.state :refer [state handle-state-change]]
-              ["localforage" :as localforage]))
+              [rpg-adventure-manager.scripts.localforageApi :as localforageApi]))
 
 (enable-console-print!)
 
+; First thing we need to do is pull all the current entities that we have saved
+(localforageApi/pull-initial-data)
 
 (defn Main []
-  (print @state)
+  (print @state) ; TODO remove in prod but nice for dev use to see that state is what we expect
   [:div.Main-Wrapper
    [:h4 {:on-click #(handle-state-change "update-current-view" "new-city")} "Add New City"]
    [:h4 {:on-click #(handle-state-change "update-current-view" "new-npc")} "Add New NPC"]
    [:h4 {:on-click #(handle-state-change "update-current-view" "new-item")} "Add New Item"]
    [:h4 {:on-click #(handle-state-change "update-current-view" "new-location")} "Add New Location"]
    [:h4 {:on-click #(handle-state-change "update-current-view" "new-hook")} "Add New Plot Hook"]
-   [:h1 {:on-click #(handle-state-change "update-state-text" "Hello New World")} (:text @state)]
-   [:h3 "Edit this and watch it change!"]
    [newcity/render state]
    [newnpc/render state]
    [newitem/render state]
--- src/rpg_adventure_manager/scripts/localforageApi.cljs
+++ src/rpg_adventure_manager/scripts/localforageApi.cljs
@@ -1,11 +1,24 @@
 (ns rpg-adventure-manager.scripts.localforageApi
+    (:require-macros [cljs.core.async.macros :refer [go]])
     (:require [rpg-adventure-manager.state :refer [handle-state-change]]
+              [cljs.core.async :as async :refer [put! chan <! >! close!]]
               ["localforage" :as localforage]))
 
 (defn add-metadata [details]
   "Adds the extra metadata we want to track on each item"
   (conj details {:used false :created (js/Date.)}))
 
+(defn get-initial-data-by-type [type]
+  (.then (.getItem localforage type) (fn [value]
+  (handle-state-change "update-entity" {:type type :value value}))))
+
+(defn pull-initial-data []
+  "this function gets all the current saved data and pushes it to the store"
+  (go (get-initial-data-by-type "cities"))
+  (go (get-initial-data-by-type "npcs"))
+  (go (get-initial-data-by-type "locations"))
+  (go (get-initial-data-by-type "items"))
+  (go (get-initial-data-by-type "hooks")))
 
 (defn add-item [type details]
   (.then (.getItem localforage type) (fn [value]
