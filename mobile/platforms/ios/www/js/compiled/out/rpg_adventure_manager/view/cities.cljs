10dc3b6f3b681e01d18db37c6033acf276f65c81
peterjewicz <peterjewicz@totalwebconnections.com>
2019-05-02 06:30:56 -0500

    Remove untracked folders in mobile

--- mobile/platforms/ios/www/js/compiled/out/rpg_adventure_manager/view/cities.cljs
+++
@@ -1,9 +0,0 @@
-(ns rpg-adventure-manager.view.cities
-    (:require [rpg-adventure-manager.state :refer [handle-state-change]]
-              [rpg-adventure-manager.scripts.localforageApi :as localforageApi]))
-
-(defn render [state]
-  (let [cities @state]
-  (fn []
-    [:div.View-Cities.viewPage {:class (:view-all-cities (:activeView @state))}
-      [:h2 "View All Cities"]])))
