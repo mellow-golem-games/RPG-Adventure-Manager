c6d6cce91135b1026b29d009cd63c785893af946
peterjewicz <peterjewicz@totalwebconnections.com>
2019-06-25 08:12:11 -0500

    remove tracking on some compiled files

--- mobile/www/js/compiled/out/rpg_adventure_manager/components/header.cljs
+++
@@ -1,7 +0,0 @@
-(ns rpg-adventure-manager.components.header
-    (:require [rpg-adventure-manager.state :refer [handle-state-change]]))
-
-(defn render []
-  (fn []
-    [:div.Header
-      [:h4 "RPG Adventure Manager"]]))
