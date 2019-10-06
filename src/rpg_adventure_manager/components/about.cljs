0bde3f3e042bdf633cbebd704c6085e44208dd09
peterjewicz <peterjewicz@totalwebconnections.com>
2019-10-06 12:12:29 -0500

    add twc to about

--- src/rpg_adventure_manager/components/about.cljs
+++ src/rpg_adventure_manager/components/about.cljs
@@ -10,7 +10,10 @@
       [:p "RPG Adventure Manage is a simple solution for managing campaigns in Table Top Campaigns.
           Its goal is to provide a simple interface to record and track the various entities present in your game.
           We're always looking to improve; for any suggestions please send us an email!"]
-      [:p "RPG Adventure Manager is built and maintained by Mellow Golem Games."]
+      [:p "RPG Adventure Manager is built and maintained by Mellow Golem Games & Total Web Connections."]
       [:p "WEBSITE: www.mellowgolemgames.com"]
+      [:p "WEBSITE: www.totalwebconnections.com.com"]
       [:br]
-      [:p str "EMAIL: "][:a {:href "Info@mellowgolemgames.com"} "Info@mellowgolemgames.com"]]])
+      [:p str "EMAIL: "][:a {:href "Info@mellowgolemgames.com"} "Info@mellowgolemgames.com"]
+      [:br]
+      [:p str "EMAIL: "][:a {:href "Info@totalwebconnections.com"} "Info@totalwebconnections.com"]]])
